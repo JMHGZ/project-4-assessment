4839f422774d6cbf5978c0d30da2091065ed69e2
Jeremiah Garza <jag_tt@yahoo.com>
2019-10-09 12:12:59 -0500

    Updates

---
+++ precache-manifest.5811ecbf65a5273b0dd3dea080195b91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "952c839e8d20ad4d79896ae23760422b",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "c76e4f161d48d2ac5044",
+    "url": "/project-4-assessment/static/css/main.2d4f9697.chunk.css"
+  },
+  {
+    "revision": "2d7523d74c0ea2522bf5",
+    "url": "/project-4-assessment/static/js/2.2863f228.chunk.js"
+  },
+  {
+    "revision": "c76e4f161d48d2ac5044",
+    "url": "/project-4-assessment/static/js/main.b55ddc7d.chunk.js"
+  },
+  {
+    "revision": "1f3c26eb599869b34eb3",
+    "url": "/project-4-assessment/static/js/runtime-main.a1778a7e.js"
+  }
+]);
